4ddb097d5c6baca8a72f5443b5da60ae918f5b75
Teddy-ff <idrees4studies@gmail.com>
2025-08-06 14:44:06 +0530

    initial commit

---
+++ src/idrees_tns_day1/FirstProgram.java
@@ -0,0 +1,9 @@
+//Demonstration of simple java program
+package idrees_tns_day1;
+import java.util.LinkedList ; //all subpackages
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
